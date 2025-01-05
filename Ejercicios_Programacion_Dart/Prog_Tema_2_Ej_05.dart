a53c112f94f3c8c2f66f920cbcce697fec70fd1c
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-05 19:49:12 +0100

    Programa completo: Ejercicio nº 5 del Tema 2

--- Ejercicios_Programacion_Dart/Prog_Tema_2_Ej_05.dart
+++ Ejercicios_Programacion_Dart/Prog_Tema_2_Ej_05.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 /*
+Ejercicio nº 5 del Tema 2 de la asignatura Programación del ciclo DAW, Curso 2022-2023
 Enunciado: Realizar un programa que lea dos valores de tipo entero y muestre por pantalla
 el valor de la suma y de la resta de esas dos variables. 
 Vamos a utilizar try catch en la suma y no en la resta para comparar qué ocurre si el usuario no introduce datos correctos. 
